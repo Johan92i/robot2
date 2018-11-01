eafda8ab4abd2211875ce8d3ea9f41b6e2001901
Johan92i <43622233+Johan92i@users.noreply.github.com>
2018-11-02 00:39:49 +0100

    adding starting project

---
+++ build/precache-manifest.2b3a0268c835f5ff6998b93852855191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f411fd9de8cb8eb5c802",
+    "url": "/static/js/main.f411fd9d.chunk.js"
+  },
+  {
+    "revision": "e55c5d92334178bd96f1",
+    "url": "/static/js/1.e55c5d92.chunk.js"
+  },
+  {
+    "revision": "f411fd9de8cb8eb5c802",
+    "url": "/static/css/main.1f4fb72c.chunk.css"
+  },
+  {
+    "revision": "e55c5d92334178bd96f1",
+    "url": "/static/css/1.f301619d.chunk.css"
+  },
+  {
+    "revision": "82479e444b0d4d89c94ea68de5a50405",
+    "url": "/index.html"
+  }
+];
